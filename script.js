35ab3c2fb00bda6a6747860fb0be667346cdadaf
zachhh14 <zactec115@gmail.com>
2022-10-15 02:01:02 +0800

    Add scores and round number

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
 const choice = ["rock", "paper", "scissors"];
-
 const buttonRock = document.querySelector("#rock");
 const buttonPaper = document.querySelector("#paper");
 const buttonScissors = document.querySelector("#scissors");
-let outputPlayer = document.querySelector(".playerImg");
-let outputComputer = document.querySelector(".computerImg");
+const outputPlayer = document.querySelector(".playerImg");
+const outputComputer = document.querySelector(".computerImg");
+const yourScore = document.querySelector("#yourScore");
+const computerScore = document.querySelector("#computerScore");
+const roundgameCounter = document.querySelector(".round");
+let yourPoints = 0;
+let computerPoints = 0;
 let playerSelection;
 let computerSelection;
+let gamenotFinish = true;
+let yourCtr = 0;
+let computerCtr = 0;
+let roundCounter = 1;
 
-buttonRock.addEventListener("click", () => {
-  playerSelection = choice[0]; //rock
-  getComputerChoice();
-  randomFunction();
-  showOutput()
-  // outputPlayer.innerHTML = `<img src="./imgs/rock.png" alt=""/>`;
-});
+function roundCheck() {
+  if (yourPoints === 3 || computerPoints === 3) {
+    gamenotFinish = false;
+  }
+}
+
+roundgameCounter.innerHTML = `Round ${roundCounter}`;
 
 function showOutput() {
   if (playerSelection === choice[0]) {
     outputPlayer.innerHTML = `<img src="./imgs/rock.png" alt="" class="imgOutput"/>`;
-  }
-  else if(playerSelection === choice[1]){
+  } else if (playerSelection === choice[1]) {
     outputPlayer.innerHTML = `<img src="./imgs/paper.png" alt="" class="imgOutput"/>`;
-  }else if(playerSelection === choice[2]){
+  } else if (playerSelection === choice[2]) {
     outputPlayer.innerHTML = `<img src="./imgs/scissors.png" alt="" class="imgOutput"/>`;
   }
   if (computerSelection === choice[0]) {
     outputComputer.innerHTML = `<img src="./imgs/rock.png" alt="" class="imgOutput"/>`;
-  }
-  else if(computerSelection === choice[1]){
+  } else if (computerSelection === choice[1]) {
     outputComputer.innerHTML = `<img src="./imgs/paper.png" alt="" class="imgOutput"/>`;
-  }else if(computerSelection === choice[2]){
+  } else if (computerSelection === choice[2]) {
     outputComputer.innerHTML = `<img src="./imgs/scissors.png" alt="" class="imgOutput"/>`;
   }
 }
 
+buttonRock.addEventListener("click", () => {
+  roundCheck();
+  if (gamenotFinish) {
+    playerSelection = choice[0]; //rock
+    getComputerChoice();
+    gameRound();
+    showOutput();
+  }
+});
+
 buttonPaper.addEventListener("click", () => {
-  playerSelection = choice[1]; //paper
-  getComputerChoice();
-  randomFunction();
-  showOutput()
+  roundCheck();
+  if (gamenotFinish) {
+    playerSelection = choice[1]; //paper
+    getComputerChoice();
+    gameRound();
+    showOutput();
+  }
 });
 
 buttonScissors.addEventListener("click", () => {
-  playerSelection = choice[2]; //scissors
-  getComputerChoice();
-  randomFunction();
-  showOutput()
+  roundCheck();
+  if (gamenotFinish) {
+    playerSelection = choice[2]; //scissors
+    getComputerChoice();
+    gameRound();
+    showOutput();
+  }
 });
-
-function randomFunction() {
-  console.log(`Player: ${playerSelection}`);
-  console.log(`Computer: ${computerSelection}`);
-  gameRound();
-}
 
 function getComputerChoice() {
   computerSelection = choice[Math.floor(Math.random() * 3)];
@@ -66,53 +82,23 @@ function gameRound() {
     (playerSelection === choice[1] && computerSelection === choice[2]) ||
     (playerSelection === choice[2] && computerSelection === choice[0])
   ) {
+    computerScore.innerHTML += `<img src="./imgs/starOutline.png" alt="" class="score" />`;
     console.log("You lose");
+    computerPoints++;
+    computerCtr++;
   } else if (playerSelection === computerSelection) {
     console.log("Tie");
   } else {
+    yourScore.innerHTML += `<img src="./imgs/starOutline.png" alt="" class="score" />`;
     console.log("You win");
+    yourPoints++;
+    yourCtr++;
   }
+  roundgameCounter.innerHTML = `Round ${roundCounter}`;
+  roundCounter++;
 }
 
 /* pseudocode
-player pick a hand
-  computer pick a hand(random)
-  rock<scissors
-  paper<rock
-  scissors<paper
-  rock===rock
-  paper===paper
-  scissors===scissors
-  5x
-
-
-
 
 
 */
-
-// function gameRound(playerSelection, computerSelection) {
-//   function getComputerChoice() {
-//     return Math.floor(Math.random() * 3);
-//   }
-
-//   computerSelection = choice[getComputerChoice()];
-
-//   if (
-//     (playerSelection === choice[0] && computerSelection === choice[1]) ||
-//     (playerSelection === choice[1] && computerSelection === choice[2]) ||
-//     (playerSelection === choice[2] && computerSelection === choice[0])
-//   ) {
-//     return `${playerSelection} < ${computerSelection} loser`;
-//   } else if (playerSelection === computerSelection) {
-//     return `${playerSelection} == ${computerSelection} tie`;
-//   } else {
-//     return `${playerSelection} > ${computerSelection} winner`;
-//   }
-// }
-
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     console.log(gameRound());
-//   }
-// }
